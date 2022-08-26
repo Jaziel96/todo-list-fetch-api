35dad862563bab72c6907c8f14848f6afc85da87
Jaziel96 <jazcov96@hotmail.com>
2022-08-26 03:01:27 +0000

    todo list api

--- src/js/component/listacontenido.jsx
+++ src/js/component/listacontenido.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import ListInput from './listainput.jsx';
 import { v4 as uuidv4 } from 'uuid';
 
@@ -9,7 +9,7 @@ import ListCounter from './listaconteo.jsx';
 import ListItem from './listaitem.jsx';
 
 function ListContainer() {
-
+    
     const [itemsArray, setItemsArray] = useState([]);
     const [itemsLength, setItemsLength] = useState(0);
 
@@ -22,11 +22,41 @@ function ListContainer() {
         setItemsLength(itemsArray.length + 1);
     }
 
-    const deleteItem = (id) => {
-        const updatedItems = itemsArray.filter(item => item.id !== id);
+    const deleteItem = (itemIndex) => {
+        const updatedItems = itemsArray.filter((item,index) => itemIndex!==index);
         setItemsArray(updatedItems);
         setItemsLength(itemsArray.length - 1)
     }
+    useEffect(async()=>{ 
+        var result= await fetch("https://assets.breatheco.de/apis/fake/todos/user/jaziel",)
+        if (!result.ok) return 
+        result=await result.json()      
+        console.log (result.result)
+        setItemsArray(result)    
+    },[]
+    )
+    useEffect(async()=>{
+        if (itemsArray.length==0) return
+        const body=JSON.stringify(itemsArray.map((item)=>{
+            return {
+                ...item,
+                done:false
+            }
+        }))
+        console.log(body)
+        var result= await fetch("https://assets.breatheco.de/apis/fake/todos/user/jaziel", {
+            method:"PUT",
+            headers: {
+                "Content-Type":"application/json"
+            },
+            body
+        })
+        if (!result.ok) return 
+        result=await result.json      
+        console.log (result.result) 
+        
+    },[itemsArray])
+
 
     return (
         <div className='list-container'>
@@ -34,16 +64,16 @@ function ListContainer() {
             <ListInput onSubmit={addItem} />
             <div className='todos-container'>
                 {
-                    itemsArray.map((item, id) =>
+                    itemsArray.map((item, index) =>
                         <ListItem
-                            key={item.id}
-                            id={item.id}
-                            itemText={item.text}
-                            deleteItem={deleteItem}
+                            key={index}
+                            itemText={item.label}
+                            deleteItem={()=>deleteItem(index)}
                         />
                     )
                 }
             </div>
+            <blockquote className="tiktok-embed" cite="https://www.tiktok.com/@beyonce/video/7133705533990636846" data-video-id="7133705533990636846" data-embed-from="oembed" style={{"max-width": "605px"}} > <section> <a target="_blank" title="@beyonce" href="https://www.tiktok.com/@beyonce?refer=embed">@beyonce</a> <p></p> <a target="_blank" title="♬ PURE/HONEY - Beyoncé" href="https://www.tiktok.com/music/PUREHONEY-7125598549672216577?refer=embed">♬ PURE/HONEY - Beyoncé</a> </section> </blockquote> <script async src="https://www.tiktok.com/embed.js"></script>
             <ListCounter listLength={itemsLength} />
         </div>
     );
